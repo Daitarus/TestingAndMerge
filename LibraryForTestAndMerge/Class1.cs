9be2880fabd6adaf786ff6515e0a85b7bcca2530
Daitarus <19fof91@gmail.ru>
2023-09-12 14:38:32 +0300

    start project

---
+++ LibraryForTestAndMerge/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LibraryForTestAndMerge
+{
+    public class Class1
+    {
+        public int Index;
+
+        public Class1(int index)
+        {
+            Index = index;
+        }
+
+        public void IndexIncrease()
+        {
+            Index++;
+        }
+    }
+}
